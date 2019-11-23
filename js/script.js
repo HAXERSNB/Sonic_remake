7f6be19729b730b11b85556794c57ceaf74e8d32
Edgar Zea <cesszea@icloud.com>
2019-11-23 16:57:40 -0600

    Drawing weapons

--- js/script.js
+++ js/script.js
@@ -1,25 +1,9 @@
 let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
-let sonic = document.getElementById('sonic-player');
-let sonicX = 0;
-let sonicY = 250;
-let sonicW = 75;
-let sonicH = 150;
 let sonicBg = document.getElementById('bcg-game');
-let sonicBgX = 0;
-let sonicBgY = 0;
-let sonicBgX2 = canvas.width;
-let sonicCoins = 0;
+let sonicImg = document.getElementById('sonic-player');
+let little = document.getElementById('little-red');
 let coins = document.getElementById('gold-ring');
-let coinsX = 776;
-let coinsY = 0;
-let coinsW = 50;
-let coinsH = 50;
-let coinsArr = [];
-let interval;
-let gravity = 10;
-let floorY = 250;
-let frames = 0;
+let weapon = document.getElementById('weapon')
 
 // let singleP = document.getElementById('single-player');
 // singleP.onclick = () => {
@@ -28,139 +12,227 @@ let frames = 0;
 
 class Sonic1P {
   constructor (canvas, sonicBg) {
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.ctx = canvas.getContext('2d');
+    this.interval;
+    this.player;
+    this.gravity = 5;
+    this.floorY = 400;
+    this.sonicBgX = 0;
+    this.sonicBgY = 0;
+    this.sonicBgX2 = canvas.width;
+    this.sonicBg = sonicBg;
+    this.sonicCoins = 0;
+    this.coinsArr = [];
+    this.enemies = [];
+    this.weapon = [];
+    this.frames = 0;
   }
+
+  startGame() {
+    this.createKeyEvents();
+    this.player = new Sonic(0, 350, 55, 125, sonicImg)
+    this.interval = setInterval(() => {
+      this.frames++
+      // if (this.player.y > this.floorY) {
+      //   this.player.y+=this.gravity;
+      // }
+      if (this.frames % 10 == 0) {
+        this.coinsArr.push(new Coin(this.width, this.floorY-25, 25, 25, coins))
+      }
+      if (this.frames % 173 == 0) {
+        this.enemies.push(new Little(this.width, this.floorY-50, 50, 50, little))
+      }
+      this.coinsArr.forEach((coin,ci) => {
+        if (this.player.checkCollition(coin)){
+          this.sonicCoins+=1;
+          this.coinsArr.splice(ci,1);
+        }
+      })
+      this.enemies.forEach((enemy, ei) => {
+        if (this.player.checkCollition(enemy)) {
+          this.sonicCoins-=10;
+          this.enemies.splice(ei,1);
+        }
+      })
+      if (this.sonicCoins % 5 == 0 && this.sonicCoins > this.weapon.length * 1) {
+        this.weapon.push(new Weapon(this.player.x + this.player.w, 50, 50, weapon))
+      }
+      this.draw()
+    },1000/60)
+  }
+
+  draw() {
+    this.ctx.clearRect(0,0,this.width, this.height);
+    this.drawBackground();
+    this.player.drawItself(this.ctx, this.gravity, this.floorY);
+    this.coinsArr.forEach(coin => {
+      coin.x-=10;
+      coin.drawItself(this.ctx)
+    })
+    this.enemies.forEach(e => {
+      e.x-=5;
+      e.drawItself(this.ctx)
+    })
+  }
+
+  drawBackground() {
+    if (this.sonicBgX <= 0) {
+      this.sonicBgX2 = this.sonicBgX + this.width
+    }
+    if (this.sonicBgX > 0) {
+      this.sonicBgX2 = this.sonicBgX - this.width
+    }
+    if (this.sonicBgX2 <= 0) {
+      this.sonicBgX = this.sonicBgX2 + this.width
+    }
+    if (this.sonicBgX2 > 0) {
+      this.sonicBgX = this.sonicBgX2 - this.width
+    }
+    this.ctx.drawImage(this.sonicBg, this.sonicBgX, this.sonicBgY, this.width, this.height);
+    this.ctx.drawImage(this.sonicBg, this.sonicBgX2, this.sonicBgY, this.width, this.height);
+  }
+
+  //Sonic moves
+  move(value) {
+   if (this.player.x + this.player.w < this.width) {
+     this.player.x-=value;
+   }
+   if (this.player.x + this.player.w > this.width) {
+     this.player.x+=value;
+   }
+   if (this.player.x < 0) {
+     this.player.x+=value;
+   }
+  }
+
+  //Background move
+  moveBack(value) {
+   this.sonicBgX+=value;
+  }
+
+  //Event Listener
+  createKeyEvents() {
+   document.onkeydown = (event) => {
+     switch(event.which) {
+       case 32:
+       // this.ctx.drawImage(this.player.x + this.player.w,this.player.w/2, 50, 50, weapon)
+       // this.weapon.drawItself(this.ctx);
+       case 37:
+       this.move(5);
+       this.moveBack(20);
+       break;
+       case 38:
+       this.player.jump(this.gravity);
+       break;
+       case 39:
+       this.move(-5);
+       this.moveBack(-20);
+       break;
+       case 40:
+       this.player.goDown(this.ctx)
+       default:
+       break;
+      }
+    }
+  document.onkeyup = (event) => {
+    switch(event.which) {
+      case 40:
+      this.player.goUp(this.ctx)
+      break;
+     }
+   }
+ }
 }
 
 class Unit {
-  constructor (enemyX, enemyY,) {}
+  constructor (x, y, w, h, src) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.src = src;
+  }
+
+  drawItself(ctx, gravity, floorY) {
+    if (this.y + this.h < floorY) {
+      this.y+=gravity
+    }
+    if (this.y + this.h > floorY) {
+      this.y = floorY - this.h;
+    }
+    ctx.drawImage(this.src, this.x, this.y, this.w, this.h);
+  }
+}
+
+class Sonic extends Unit {
+  constructor (x, y, w, h, src) {
+    super (x, y, w, h, src)
+    this.h = h;
+  }
+
+  goDown (ctx) {
+    this.h = 70
+  }
+
+  goUp (ctx) {
+    this.h = 125;
+  }
+
+  jump (gravity) {
+    if (this.y > this.h/2) {
+      this.y-=200-gravity
+    }
+  }
+
+  checkCollition(unit) {
+    if (this.x + (this.w/2) > unit.x && unit.x + unit.w > this.x && this.y < unit.y + unit.h && this.y + this.h > unit.y) {
+      return true;
+    }
+    return false;
+  }
+}
+
+class Coin extends Unit {
+  constructor (x, y, w, h, src) {
+    super(x, y, w, h, src)
+    this.y = Math.floor(Math.random()* (326 - 126 + 1) + 126);
+  }
+  drawItself(ctx) {
+    ctx.drawImage(this.src, this.x, this.y, this.w, this.h);
+  }
+}
+
+class Little extends Unit {
+  constructor (x, y, w, h, src) {
+    super (x, y, w, h, src)
+    this.y = Math.floor(Math.random() * 400);
+  }
+  drawItself(ctx) {
+    ctx.drawImage(this.src, this.x, this.y, this.w, this.h);
+  }
+}
+
+class Weapon extends Unit {
+  drawItself(ctx) {
+    this.weapon.forEach(wpn => {
+      wpn+=10;
+      ctx.drawImage(this.src, this.x, this.y, this.w, this.h);
+    })
+  }
+
+  checkCollition(unit) {
+    if (this.x + (this.w/2) > unit.x && unit.x + unit.w > this.x && this.y < unit.y + unit.h && this.y + this.h > unit.y) {
+      return true;
+    }
+    return false;
+  }
 }
 
 class Sonic2P {
   constructor (canvas, sonicBg) {}
 }
 
-class Coin {
-  constructor(img, x, y) {
-    this.x = x;
-    this.y = y;
-    this.img = img;
-  }
-}
-
-function startGame() {
-  createKeyEvents();
-  interval = setInterval(() => {
-    frames++
-    if (frames % 50 == 0) {
-      coinsArr.push(new Coin(coins, coinsX, coinsY))
-    }
-    coinsArr.forEach(coin => {
-      if (checkCollition(coin)) {
-        sonicCoins+=1;
-        console.log(sonicCoins)
-      }
-    })
-    draw()
-  },1000/60)
-}
-
-//Draw Everything
-function draw() {
-  ctx.clearRect(0,0,canvas.width, canvas.height);
-  drawBackground();
-  drawSonic();
-  drawCoins();
-}
-
-//Drawing Sonic
-function drawSonic() {
-  if (sonicY < floorY) {
-    sonicY+=gravity
-  }
-  if (sonicY > floorY) {
-    sonicY = floorY - sonicH;
-  }
-  ctx.drawImage(sonic, sonicX, sonicY, sonicW, sonicH);
-}
-
-//Draw Coins
-function drawCoins() {
-  coinsY = Math.floor(Math.random()* (326 - 126 + 1) + 126);
-  coinsArr.forEach(coin => {
-    coin.x-=5
-    ctx.drawImage(coin.img, coin.x, coin.y, coinsW, coinsH);
-    if (checkCollition()) {
-      sonicCoins+=1;
-      console.log(sonicCoins)
-    }
-  })
-}
-
-//Drawing Background
-function drawBackground() {
-  if (sonicBgX < 0) {
-    sonicBgX2 = sonicBgX + canvas.width
-  }
-  if (sonicBgX > 0) {
-    sonicBgX2 = sonicBgX - canvas.width
-  }
-  if (sonicBgX2 < 0) {
-    sonicBgX = sonicBgX2 + canvas.width
-  }
-  if (sonicBgX2 > 0) {
-    sonicBgX = sonicBgX2 - canvas.width
-  }
-  ctx.drawImage(sonicBg, sonicBgX, sonicBgY, canvas.width, canvas.height);
-  ctx.drawImage(sonicBg, sonicBgX2, sonicBgY, canvas.width, canvas.height);
-}
-
-//Event Listener
-function createKeyEvents() {
- document.onkeydown = (event) => {
-   switch(event.which) {
-     case 37:
-       move(5);
-       moveBack(10);
-       break;
-     case 38:
-       sonicY-=200
-       break;
-     case 39:
-       move(-5);
-       moveBack(-10);
-       break;
-     default:
-       break;
-   }
- }
-}
-
-//Collision
-function checkCollition(unit) {
-  if (sonicX + sonicW > Coin.x && Coin.x + coinsW > sonicX && sonicY + sonicH > Coin.y && sonicY < Coin.y + coinsH) {
-    console.log('hi')
-    return true;
-  }
-  return false;
-}
-
-//Sonic moves
-function move(value) {
- if (sonicX + sonicW < canvas.width) {
-   sonicX-=value;
- }
- if (sonicX + sonicW > canvas.width) {
-   sonicX+=value;
- }
- if (sonicX < 0) {
-   sonicX+=value;
- }
-}
-
-//Background move
-function moveBack(value) {
- sonicBgX+=value;
-}
-
-
-startGame();
+let game = new Sonic1P(canvas, sonicBg);
+game.startGame();
